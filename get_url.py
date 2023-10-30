2fa75da5567bc75404eec7bc0cc4530455209e8a
CliffordCapital <147798932+CliffordCapital@users.noreply.github.com>
2023-10-30 14:12:25 +0800

    Update get_url.py

--- get_url.py
+++ get_url.py
@@ -1,7 +1,6 @@
-
 #EDIT FILE NAME
-input_path = 'get_url.xlsx'
-output_path = 'scraped_urls.csv'
+input_path = 'Scraper_input_GH_Nov_2023.xlsx'
+output_path = 'scraped_urls_data.csv'
 output_blocked_path = 'blocked_regions.csv'
 
 from selenium import webdriver
@@ -10,9 +9,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import multiprocessing
+from multiprocessing import Manager
+
 
 blocked = []
-def scrape_region(region):
+def scrape_region(region, blocked):
     try:
         options = Options()
         options.add_argument("--no-sandbox")
@@ -28,8 +29,8 @@ def scrape_region(region):
         driver.find_element(By.XPATH, '//*[@id="myModal"]/div/div/div[2]/button[2]').click()
         driver.find_element(By.XPATH, '/html/body/div[2]/div/form/span[2]/input[2]').send_keys(region)
         driver.implicitly_wait(3)
-        #driver.find_element(By.XPATH, '//*[@id="search"]/span[2]/div/div/div[1]').click() #GET THE FIRST DROP DOWN
-        driver.find_element(By.XPATH, '//*[@id="search"]/span[2]/div/div/div[2]').click() # GET THET SECOND DROP DOWN
+        driver.find_element(By.XPATH, '//*[@id="search"]/span[2]/div/div/div[1]').click() #GET THE FIRST DROP DOWN
+        #driver.find_element(By.XPATH, '//*[@id="search"]/span[2]/div/div/div[2]').click() # GET THET SECOND DROP DOWN
         driver.implicitly_wait(3)
         
 
@@ -41,20 +42,23 @@ def scrape_region(region):
         return {'region': region, 'url': scraped_url}
 
     except Exception as e:
-        print('Blocked', region)
         blocked.append(region)
+        print('Blocked', region)
         return {'region': region, 'url': None}
 
 # RUN THIS
 if __name__ == '__main__':
     df = pd.read_excel(input_path)
-    df = df[90:] #EDIT THIS; BATCH NUMBER
+    df = df[:10] #EDIT THIS; BATCH NUMBER
     regions_to_scrape = df['region'].tolist()
 
     num_processes = multiprocessing.cpu_count()  # Use the number of available CPU cores
+    manager = Manager()
+    blocked = manager.list()
     pool = multiprocessing.Pool(processes=num_processes)
 
-    output = pool.map(scrape_region, regions_to_scrape)
+    #output = pool.map(scrape_region, regions_to_scrape)
+    output = pool.starmap(scrape_region, [(region, blocked) for region in regions_to_scrape])
     pool.close()
     pool.join()
 
@@ -64,9 +68,11 @@ if __name__ == '__main__':
 
     # Create DataFrames from the processed output
     output_df = pd.DataFrame(result_output, columns=['region', 'url'])
-    blocked_df = pd.DataFrame(blocked)
-    print(blocked_df)
-    print(blocked)
+    output_df = output_df.merge(df[['region', 'region_hl', 'country']], on = 'region', how = 'left')
+    blocked_df = pd.DataFrame(list(blocked), columns=['region'])
+    blocked_df = blocked_df.merge(df[['region', 'region_hl', 'country']], on = 'region', how = 'left')
+    #print(blocked_df)
+    #print(blocked)
 
 
     # Save DataFrames to CSV files
